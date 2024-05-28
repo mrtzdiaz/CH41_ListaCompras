912200ed81df7a1ea42504f8b1dd17eae9a93d53
Daniel <mrtzdiazdaniel@gmail.com>
2024-05-28 13:00:18 -0600

    Se agregan las validaciones de los campos

--- js/main.js
+++ js/main.js
@@ -1,1 +1,50 @@
 // El código va aquí -> 
+let btnAgregar = document.getElementById("btnAgregar");
+let btnClear = document.getElementById("btnClear");
+let txtNombre = document.getElementById("Name");
+let txtNumero = document.getElementById("Number");
+let tabla = document.getElementsByTagName("tbody");
+let alerta = document.getElementById("alertValidacionesTexto");
+let recuadro_alerta = document.getElementById("alertValidaciones");
+
+function validarCantidad(){
+    if (txtNumero.value.length == 0){
+        return false;
+    }
+    return true;
+}
+
+
+btnAgregar.addEventListener("click", function (event) {
+    event.preventDefault();
+    recuadro_alerta.style.display = "none";
+    alerta.innerHTML = "";
+    txtNombre.style.border = "";
+    txtNumero.style.border = "";
+
+
+    if (txtNombre.value.length < 3) {
+        recuadro_alerta.style.display = "block";
+        alerta.innerHTML = "El <strong>nombre</strong> no es correcto<br>";
+        txtNombre.style.border = "solid red medium";
+        
+
+    }
+    
+    if (! validarCantidad()) {
+        recuadro_alerta.style.display = "block";
+        alerta.innerHTML += "La <strong>cantidad</strong> no es correcta";
+        txtNumero.style.border = "solid red medium";
+
+    } else {
+        let producto = txtNombre.value;
+        let cantidad = txtNumero.value;
+    }
+
+});
+
+btnClear.addEventListener("click", function (event) {
+    event.preventDefault();
+    txtNombre.value = "";
+    txtNumero.value = "";
+});
